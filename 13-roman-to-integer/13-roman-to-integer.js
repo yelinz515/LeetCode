bf25597fd6afb2c90ffac1dfbb88c65c7470dd9e
yelinz515 <62506973+yelinz515@users.noreply.github.com>
2022-07-20 15:41:45 +0900

    Time: 151 ms (83.38%), Space: 47.1 MB (68.10%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const strArr = s.split('');
+    const numArr = strArr.map(s => {
+        switch(s) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+        }
+    })
+
+    let sum = numArr[numArr.length - 1];
+    for (let i = numArr.length - 1; i >= 0; i--) {
+        if (i-1 < 0) {
+            break;
+        }
+
+        if (sum > numArr[i-1] && numArr[i] > numArr[i-1]) {
+            sum -= numArr[i-1];
+        } else {
+          sum += numArr[i-1];
+        }
+    }
+
+    return sum;
+    
+};
