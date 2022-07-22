612777cdffccfaca0d826959239f69120ddbaab5
yelinz515 <62506973+yelinz515@users.noreply.github.com>
2022-07-22 17:38:17 +0900

    Time: 5379 ms (17.74%), Space: 42.2 MB (20.70%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+    let result =0;
+    for(let i =low; i <=high; i++){
+        if(i % 2 !== 0){
+            result++
+        }
+    }
+    return result;
+};
