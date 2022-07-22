cc430aa03d64c8115f0f2d17e7b681d6f0ff41c8
yelinz515 <62506973+yelinz515@users.noreply.github.com>
2022-07-22 17:13:19 +0900

    Time: 90 ms (43.85%), Space: 42.6 MB (18.27%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left =0;
+    let right = nums.length-1; //3
+    
+    while(left <= right){
+        let mid = left + Math.floor((right -left)/2)//1
+            if(target === nums[mid]){
+                return mid;
+            }else if (target > nums[mid]) {
+                left = mid + 1;
+            }else {
+                right = mid -1;
+            }
+    }
+    return left
+};
